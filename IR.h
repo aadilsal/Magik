45042590546e5c663672bc18981a1127e2c46f62
Aadil Salman Butt <salmanaadil52@gmail.com>
2025-04-07 21:39:04 +0500

    LLVMPRINT ERROR REMIAINs

--- IR.h
+++ IR.h
@@ -10,6 +10,7 @@
 #include <llvm/IR/Value.h>
 #include <llvm/Support/raw_ostream.h>
 #include "ssc.tab.h"
+extern FILE *yyin;  // Flex's input file pointer
 
 using namespace llvm;
 
@@ -51,18 +52,44 @@ static IRBuilder<> builder(context);
 static Function *currentFunction = nullptr;
 static BasicBlock *currentBlock = nullptr;
 
-/**
-* Initialize LLVM context and module
-*/
+
 static void initLLVM() {
     module = new Module("top", context);
+    FunctionType *mainTy = FunctionType::get(builder.getInt32Ty(), false);
+    mainFunction = Function::Create(mainTy, Function::ExternalLinkage, "main", module);
+    BasicBlock *entry = BasicBlock::Create(context, "entry", mainFunction);
+    builder.SetInsertPoint(entry);
 }
 
-// [Previous implementations remain the same...]
+void printLLVMIR() {
+    if (module) {
+        module->print(llvm::errs(), nullptr);
+    }
+}
+void addReturnInstr() {
+    builder.CreateRet(ConstantInt::get(context, APInt(32, 0)));
+}
 
-/**
-* Create a function with given return type, name, parameters and body
-*/
+void printString(const char *str) {
+    Value *strValue = builder.CreateGlobalStringPtr(str);
+    printfLLVM("%s\n", strValue);
+}
+
+void printDouble(Value *value) {
+    printfLLVM("%f\n", value);
+}
+
+void printfLLVM(const char *format, Value *inputValue) {
+    Function *printfFunc = module->getFunction("printf");
+    if(!printfFunc) {
+        FunctionType *printfTy = FunctionType::get(builder.getInt32Ty(), 
+            PointerType::get(builder.getInt8Ty(), 0), true);
+        printfFunc = Function::Create(printfTy, Function::ExternalLinkage, "printf", module);
+    }
+    Value *formatVal = builder.CreateGlobalStringPtr(format);
+    builder.CreateCall(printfFunc, {formatVal, inputValue}, "printfCall");
+}
+
 Value* createFunction(int returnType, const char* name, std::vector<llvm::Value*>* params, Value* body) {
     // Determine return type
     Type *retType;
@@ -210,18 +237,88 @@ void createReturnStatement(Value* value) {
     }
 }
 
-/**
-* Create a function call
-*/
-/**Value* createFunctionCall(const char* name, Value* args) {
-    Function *callee = FunctionTable[name];
-    if (!callee) {
-        yyerror("undefined function");
-        exit(EXIT_FAILURE);
+// Error handling
+void yyerror(const char *err) {
+    fprintf(stderr, "\n%s\n", err);
+}
+
+// Variable and symbol table operations
+Value* getFromSymbolTable(const char *id) {
+    std::string name(id);
+    if(SymbolTable.find(name) != SymbolTable.end()) {
+        return SymbolTable[name];
+    } else {
+        Value* defaultValue = builder.CreateAlloca(builder.getDoubleTy(), nullptr, name);
+        SymbolTable[name] = defaultValue;
+        return defaultValue;
+    }
+}
+
+void setDouble(const char *id, Value* value) {
+    Value *ptr = getFromSymbolTable(id);
+    builder.CreateStore(value, ptr);
+}
+
+void declareVariable(int type, const char* name) {
+    Type* varType = type == TYPE_INT ? builder.getInt32Ty() : builder.getDoubleTy();
+    Value* var = builder.CreateAlloca(varType, nullptr, name);
+    SymbolTable[name] = var;
+}
+
+void declareAndAssignVariable(int type, const char* name, Value* value) {
+    declareVariable(type, name);
+    setDouble(name, value);
+}
+
+// Parameter handling
+Value* createParameter(int type, const char* name) {
+    Type* paramType = type == TYPE_INT ? builder.getInt32Ty() : builder.getDoubleTy();
+    return builder.CreateAlloca(paramType, nullptr, name);
+}
+
+// Binary operations
+Value* performBinaryOperation(Value* lhs, Value* rhs, int op) {
+    switch (op) {
+        case '+': return builder.CreateFAdd(lhs, rhs, "fadd");
+        case '-': return builder.CreateFSub(lhs, rhs, "fsub");
+        case '*': return builder.CreateFMul(lhs, rhs, "fmul");
+        case '/': return builder.CreateFDiv(lhs, rhs, "fdiv");
+        default: yyerror("illegal binary operation"); exit(EXIT_FAILURE);
+    }
+}
+
+// Constant creation
+Value* createDoubleConstant(double val) {
+    return ConstantFP::get(context, APFloat(val));
+}
+
+int main(int argc, char** argv) {
+    extern FILE *yyin;  // Declaration of Flex's input file pointer
+    
+    // Initialize LLVM
+    initLLVM();
+    
+    // Set input source
+    if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            fprintf(stderr, "Could not open input file: %s\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+    } else {
+        yyin = stdin;
     }
     
-    // Handle arguments here (simplified)
-    return builder.CreateCall(callee, {}, "calltmp");
+    // Run parser
+    int parserResult = yyparse();
+    
+    // Print generated IR
+    printLLVMIR();
+    
+    // Clean up
+    if (yyin != stdin) {
+        fclose(yyin);
+    }
+    
+    return parserResult ? EXIT_FAILURE : EXIT_SUCCESS;
 }
-*/
-
