53aac6528a54d8be33f500b163ef27011c5d528e
aadilsal <salmanaadil52@gmail.com>
2025-04-14 23:01:36 +0500

    Minor edits(if else working)

--- IR.h
+++ IR.h
@@ -1,9 +1,5 @@
 #include <string>
 #include <map>
-<<<<<<< HEAD
-#include <vector>
-=======
->>>>>>> master
 #include <stdio.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -12,376 +8,28 @@
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Value.h>
 #include <llvm/Support/raw_ostream.h>
-<<<<<<< HEAD
-#include "ssc.tab.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/DerivedTypes.h"
-
-extern FILE *yyin;  // Flex's input file pointer
-static llvm::Function* mainFunction = nullptr;
 
 using namespace llvm;
 
-// Type definitions
-#define TYPE_INT 0
-#define TYPE_DOUBLE 1
-#define TYPE_VOID 2
-
-
-// Function declarations
-void printfLLVM(const char* format, llvm::Value* value);
-void printfLLVM(const char* format, const char* str);
-=======
-
-using namespace llvm;
-
->>>>>>> master
 Value* getFromSymbolTable(const char *id);
 void setDouble(const char *id, Value* value);
 void printString(const char *str);
 void printDouble(Value* value);
 Value* performBinaryOperation(Value* lhs, Value* rhs, int op);
-<<<<<<< HEAD
-Value* createComparison(Value* lhs, Value* rhs, int op);
-=======
 Value* createComparison(Value* lhs, Value* rhs, const char *op);
 void handleIf(Value* cond, Value* thenVal);
 void handleIfElse(Value* cond, Value* thenVal, Value* elseVal);
->>>>>>> master
 void yyerror(const char *err);
 static void initLLVM();
 void printLLVMIR();
 void addReturnInstr();
 Value* createDoubleConstant(double val);
-<<<<<<< HEAD
-Value* createFunction(int returnType, const char* name, std::vector<llvm::Value*>* params, Value* body);
-Value* createParameter(int type, const char* name);
-void declareVariable(int type, const char* name);
-void declareAndAssignVariable(int type, const char* name, Value* value);
-void createIfStatement(Value* condition, Value* thenBlock, Value* elseBlock);
-void createForLoop(Value* init, Value* condition, Value* increment, Value* body);
-void createReturnStatement(Value* value);
-Value* createFunctionCall(const char* name, std::vector<llvm::Value*>* args);
-
-
-// Global variables
-static std::map<std::string, Value *> SymbolTable;
-static std::map<std::string, Function *> FunctionTable;
-=======
 
 static std::map<std::string, Value *> SymbolTable;
->>>>>>> master
 
 static LLVMContext context;
 static Module *module = nullptr;
 static IRBuilder<> builder(context);
-<<<<<<< HEAD
-static Function *currentFunction = nullptr;
-static BasicBlock *currentBlock = nullptr;
-
-
-static void initLLVM() {
-    module = new Module("top", context);
-    // FunctionType *mainTy = FunctionType::get(builder.getInt32Ty(), false);
-   // mainFunction = Function::Create(mainTy, Function::ExternalLinkage, "main", module);
-   // BasicBlock *entry = BasicBlock::Create(context, "entry", mainFunction);
-   // builder.SetInsertPoint(entry);
-}
-
-void printfLLVM(const char* format, llvm::Value* value) {
-    std::vector<llvm::Type*> printfArgs;
-    printfArgs.push_back(llvm::PointerType::get(llvm::Type::getInt8Ty(context), 0));
-    printfArgs.push_back(llvm::Type::getDoubleTy(context));
-    llvm::FunctionType* printfType = llvm::FunctionType::get(
-        llvm::Type::getInt32Ty(context), printfArgs, true);
-    
-    llvm::Function* printfFunc = llvm::Function::Create(
-        printfType, llvm::Function::ExternalLinkage, "printf", module);
-    
-    std::vector<llvm::Value*> args;
-    args.push_back(builder.CreateGlobalStringPtr(format));
-    args.push_back(value);
-    builder.CreateCall(printfFunc, args);
-}
-
-void printfLLVM(const char* format, const char* str) {
-    std::vector<llvm::Type*> printfArgs;
-    printfArgs.push_back(llvm::PointerType::get(llvm::Type::getInt8Ty(context), 0));
-    printfArgs.push_back(llvm::PointerType::get(llvm::Type::getInt8Ty(context), 0));
-    llvm::FunctionType* printfType = llvm::FunctionType::get(
-        llvm::Type::getInt32Ty(context), printfArgs, true);
-    
-    llvm::Function* printfFunc = llvm::Function::Create(
-        printfType, llvm::Function::ExternalLinkage, "printf", module);
-    
-    std::vector<llvm::Value*> args;
-    args.push_back(builder.CreateGlobalStringPtr(format));
-    args.push_back(builder.CreateGlobalStringPtr(str));
-    builder.CreateCall(printfFunc, args);
-}
-
-void addReturnInstr() {
-    builder.CreateRet(ConstantInt::get(context, APInt(32, 0)));
-}
-
-void printString(const char *str) {
-    Value *strValue = builder.CreateGlobalStringPtr(str);
-    printfLLVM("%s\n", strValue);
-}
-
-void printDouble(Value *value) {
-    printfLLVM("%f\n", value);
-}
-/*
-void printfLLVM(const char *format, Value *inputValue) {
-    Function *printfFunc = module->getFunction("printf");
-    if(!printfFunc) {
-        FunctionType *printfTy = FunctionType::get(builder.getInt32Ty(), 
-            PointerType::get(builder.getInt8Ty(), 0), true);
-        printfFunc = Function::Create(printfTy, Function::ExternalLinkage, "printf", module);
-    }
-    Value *formatVal = builder.CreateGlobalStringPtr(format);
-    builder.CreateCall(printfFunc, {formatVal, inputValue}, "printfCall");
-}
-*/
-Value* createFunction(int returnType, const char* name, std::vector<llvm::Value*>* params, Value* body) {
-    // Determine return type
-    Type *retType;
-    switch(returnType) {
-        case TYPE_INT: retType = builder.getInt32Ty(); break;
-        case TYPE_DOUBLE: retType = builder.getDoubleTy(); break;
-        case TYPE_VOID: 
-        default: retType = builder.getVoidTy();
-    }
-    
-    // Create parameter types vector
-    std::vector<Type*> paramTypes;
-    if (params) {
-        for (auto param : *params) {
-            paramTypes.push_back(param->getType());
-        }
-    }
-    
-    // Create function type
-    FunctionType *funcType = FunctionType::get(retType, paramTypes, false);
-    Function *func = Function::Create(funcType, Function::ExternalLinkage, name, module);
-    
-    // Add to function table
-    FunctionTable[name] = func;
-    
-    // Create entry block
-    BasicBlock *entry = BasicBlock::Create(context, "entry", func);
-    builder.SetInsertPoint(entry);
-    
-    // Set current function and block
-    currentFunction = func;
-    currentBlock = entry;
-    
-    return func;
-}
-
-Value* createFunctionCall(const char* name, std::vector<llvm::Value*>* args) {
-    Function *callee = FunctionTable[name];
-    if (!callee) {
-        yyerror("undefined function");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (args) {
-        return builder.CreateCall(callee, *args, "calltmp");
-    } else {
-        return builder.CreateCall(callee, {}, "calltmp");
-    }
-}
-
-
-
-Value* createComparison(Value* lhs, Value* rhs, int op) {
-    switch(op) {
-        case '<': return builder.CreateFCmpULT(lhs, rhs, "cmplt");
-        case '>': return builder.CreateFCmpUGT(lhs, rhs, "cmpgt");
-        case tok_le: return builder.CreateFCmpULE(lhs, rhs, "cmple");
-        case tok_ge: return builder.CreateFCmpUGE(lhs, rhs, "cmpge");
-        case tok_eq: return builder.CreateFCmpUEQ(lhs, rhs, "cmpeq");
-        case tok_ne: return builder.CreateFCmpUNE(lhs, rhs, "cmpne");
-        default: yyerror("invalid comparison operator"); exit(EXIT_FAILURE);
-    }
-}
-/**
-* Create an if-else statement
-*/
-void createIfStatement(Value* condition, Value* thenBlock, Value* elseBlock) {
-    // Create blocks for then and else parts
-    BasicBlock *thenBB = BasicBlock::Create(context, "then", currentFunction);
-    BasicBlock *elseBB = elseBlock ? BasicBlock::Create(context, "else", currentFunction) : nullptr;
-    BasicBlock *mergeBB = BasicBlock::Create(context, "ifcont", currentFunction);
-    
-    // Create conditional branch
-    builder.CreateCondBr(condition, thenBB, elseBB ? elseBB : mergeBB);
-    
-    // Emit then block
-    builder.SetInsertPoint(thenBB);
-    // Code for then block would be generated here
-    builder.CreateBr(mergeBB);
-    
-    // Emit else block if exists
-    if (elseBB) {
-        builder.SetInsertPoint(elseBB);
-        // Code for else block would be generated here
-        builder.CreateBr(mergeBB);
-    }
-    
-    // Continue with merge block
-    builder.SetInsertPoint(mergeBB);
-}
-
-/**
-* Create a for loop
-*/
-void createForLoop(Value* init, Value* condition, Value* increment, Value* body) {
-    // Create basic blocks
-    BasicBlock *preheaderBB = builder.GetInsertBlock();
-    BasicBlock *loopBB = BasicBlock::Create(context, "loop", currentFunction);
-    BasicBlock *bodyBB = BasicBlock::Create(context, "loopbody", currentFunction);
-    BasicBlock *afterBB = BasicBlock::Create(context, "afterloop", currentFunction);
-    
-    // Emit initialization code
-    if (init) {
-        // Code for initialization would be generated here
-    }
-    
-    // Branch to loop condition check
-    builder.CreateBr(loopBB);
-    
-    // Emit loop condition check
-    builder.SetInsertPoint(loopBB);
-    if (condition) {
-        // Create conditional branch
-        builder.CreateCondBr(condition, bodyBB, afterBB);
-    } else {
-        // Infinite loop if no condition
-        builder.CreateBr(bodyBB);
-    }
-    
-    // Emit loop body
-    builder.SetInsertPoint(bodyBB);
-    // Code for loop body would be generated here
-    if (increment) {
-        // Code for increment would be generated here
-    }
-    builder.CreateBr(loopBB);
-    
-    // Continue with after loop block
-    builder.SetInsertPoint(afterBB);
-}
-
-/**
-* Create a return statement
-*/
-void createReturnStatement(Value* value) {
-    if (value) {
-        builder.CreateRet(value);
-    } else {
-        if (currentFunction->getReturnType()->isVoidTy()) {
-            builder.CreateRetVoid();
-        } else {
-            yyerror("non-void function must return a value");
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-// Error handling
-void yyerror(const char *err) {
-    fprintf(stderr, "\n%s\n", err);
-}
-
-// Variable and symbol table operations
-Value* getFromSymbolTable(const char *id) {
-    std::string name(id);
-    if(SymbolTable.find(name) != SymbolTable.end()) {
-        return SymbolTable[name];
-    } else {
-        Value* defaultValue = builder.CreateAlloca(builder.getDoubleTy(), nullptr, name);
-        SymbolTable[name] = defaultValue;
-        return defaultValue;
-    }
-}
-
-void setDouble(const char *id, Value* value) {
-    Value *ptr = getFromSymbolTable(id);
-    builder.CreateStore(value, ptr);
-}
-
-void declareVariable(int type, const char* name) {
-    Type* varType = type == TYPE_INT ? builder.getInt32Ty() : builder.getDoubleTy();
-    Value* var = builder.CreateAlloca(varType, nullptr, name);
-    SymbolTable[name] = var;
-}
-
-void declareAndAssignVariable(int type, const char* name, Value* value) {
-    declareVariable(type, name);
-    setDouble(name, value);
-}
-
-// Parameter handling
-Value* createParameter(int type, const char* name) {
-    Type* paramType = type == TYPE_INT ? builder.getInt32Ty() : builder.getDoubleTy();
-    return builder.CreateAlloca(paramType, nullptr, name);
-}
-
-// Binary operations
-Value* performBinaryOperation(Value* lhs, Value* rhs, int op) {
-    switch (op) {
-        case '+': return builder.CreateFAdd(lhs, rhs, "fadd");
-        case '-': return builder.CreateFSub(lhs, rhs, "fsub");
-        case '*': return builder.CreateFMul(lhs, rhs, "fmul");
-        case '/': return builder.CreateFDiv(lhs, rhs, "fdiv");
-        default: yyerror("illegal binary operation"); exit(EXIT_FAILURE);
-    }
-}
-
-// Constant creation
-Value* createDoubleConstant(double val) {
-    return ConstantFP::get(context, APFloat(val));
-}
-
-void printLLVMIR() {
-    if (module) {
-        module->print(llvm::outs(), nullptr);
-    }
-}
-
-int main(int argc, char** argv) {
-    extern FILE *yyin;  // Declaration of Flex's input file pointer
-    
-    // Initialize LLVM
-    initLLVM();
-    
-    // Set input source
-    if (argc > 1) {
-        yyin = fopen(argv[1], "r");
-        if (!yyin) {
-            fprintf(stderr, "Could not open input file: %s\n", argv[1]);
-            return EXIT_FAILURE;
-        }
-    } else {
-        yyin = stdin;
-    }
-    
-    // Run parser
-    int parserResult = yyparse();
-    
-    // Print generated IR
-    printLLVMIR();
-    
-    // Clean up
-    if (yyin != stdin) {
-        fclose(yyin);
-    }
-    
-    return parserResult ? EXIT_FAILURE : EXIT_SUCCESS;
-=======
 static Function *mainFunction = nullptr;
 
 static void initLLVM() {
@@ -426,7 +74,7 @@ void handleIf(Value* cond) {
     builder.CreateCondBr(cond, thenBB, mergeBB);
     
     builder.SetInsertPoint(thenBB);
-    // Then block code will be generated here
+    
     builder.CreateBr(mergeBB);
     
     builder.SetInsertPoint(mergeBB);
@@ -462,8 +110,7 @@ void handleIfElseIf(Value* cond) {
     builder.CreateBr(mergeBB);
     
     builder.SetInsertPoint(elseBB);
-    // Else block will contain the next if-else (handled automatically)
-    // No need to create another branch here
+    
 }
 void printLLVMIR() {
 	module->print(errs(), nullptr);
@@ -512,5 +159,4 @@ Value* performBinaryOperation(Value* lhs, Value* rhs, int op) {
 		case '/': return builder.CreateFDiv(lhs, rhs, "fdiv");
 		default: yyerror("illegal binary operation"); exit(EXIT_FAILURE);
 	}
->>>>>>> master
 }
--- ssc.y
+++ ssc.y
@@ -1,28 +1,13 @@
 %{
     #include <stdio.h>
     #include <stdlib.h>
-<<<<<<< HEAD
-    #include "IR.h"
-    
-    
-    extern llvm::Function* mainFunction; 
-    extern void printfLLVM(const char* format, llvm::Value* value);
-    extern void printfLLVM(const char* format, const char* str);
-    
-    
-=======
     #include <string.h>
     #include "IR.h"
     
->>>>>>> master
     extern int yyparse();
     extern int yylex();
     extern FILE *yyin;
     
-<<<<<<< HEAD
-    //#define DEBUGBISON
-=======
->>>>>>> master
     #ifdef DEBUGBISON
         #define debugBison(a) (printf("\n%d \n",a))
     #else
@@ -34,136 +19,14 @@
     char *identifier;
     double double_literal;
     char *string_literal;
-<<<<<<< HEAD
-    llvm::Value* value; 
-    int type;
-    std::vector<llvm::Value*>* param_list;
-=======
     char *op;
     llvm::Value* value; 
->>>>>>> master
 }
 
 %token tok_printd
 %token tok_prints
 %token tok_if
 %token tok_else
-<<<<<<< HEAD
-%token tok_for
-%token tok_return
-%token tok_int
-%token tok_double
-%token tok_void
-%token IFX
-
-%token tok_le    // <=
-%token tok_ge    // >=
-%token tok_eq    // ==
-%token tok_ne    // !=
-%token tok_function
-
-%token <identifier> tok_identifier
-%token <double_literal> tok_double_literal
-%token <string_literal> tok_string_literal
-
-%type <value> term expression statement expression_statement return_statement
-%type <type> type_specifier
-%type <param_list> parameter_list argument_list
-%type <value> parameter function compound_statement selection_statement iteration_statement
-%precedence IFX
-
-%start program
-
-%%
-
-program:    /* empty */
-            | program function
-            | program declaration
-            | program statement
-            ;
-function:   type_specifier tok_identifier '(' parameter_list ')' compound_statement
-            { debugBison(20); $$ = createFunction($1, $2, $4, $6); free($2); }
-            | tok_identifier '(' parameter_list ')' compound_statement
-            ;
-
-parameter_list: /* empty */               { debugBison(21); $$ = new std::vector<llvm::Value*>(); }
-            | parameter                   { debugBison(22); $$ = new std::vector<llvm::Value*>(); $$->push_back($1); }
-            | parameter_list ',' parameter { debugBison(23); $1->push_back($3); $$ = $1; }
-            ;
-
-parameter:   type_specifier tok_identifier { debugBison(24); $$ = createParameter($1, $2); free($2); }
-            ;
-
-declaration: type_specifier tok_identifier ';' 
-            { debugBison(25); declareVariable($1, $2); free($2); }
-            | type_specifier tok_identifier '=' expression ';'
-            { debugBison(26); declareAndAssignVariable($1, $2, $4); free($2); }
-            ;
-
-type_specifier: tok_int      { debugBison(27); $$ = TYPE_INT; }
-              | tok_double   { debugBison(28); $$ = TYPE_DOUBLE; }
-              | tok_void     { debugBison(29); $$ = TYPE_VOID; }
-              ;
-statement:   compound_statement
-            | expression_statement
-            | selection_statement
-            | iteration_statement
-            | return_statement
-            | tok_prints '(' tok_string_literal ')' ';' { printString($3); free($3); }
-            | tok_printd '(' expression ')' ';' { printDouble($3); }
-            ;
-
-compound_statement: '{' statement_list '}' { debugBison(30); $$ = nullptr; }
-                  ;
-
-statement_list: /* empty */
-              | statement_list statement
-              ;
-
-expression_statement: expression ';' { debugBison(31); $$ = $1; }
-                    | ';'            { debugBison(32); $$ = nullptr; }
-                    ;
-
-selection_statement: tok_if '(' expression ')' statement %prec IFX
-                   { debugBison(33); createIfStatement($3, $5, nullptr); $$ = $3; }
-                   | tok_if '(' expression ')' statement tok_else statement
-                   { debugBison(34); createIfStatement($3, $5, $7); $$ = $3; }
-                   ;
-
-iteration_statement: tok_for '(' expression_statement expression_statement expression ')' statement
-                   { debugBison(35); createForLoop($3, $4, $5, $7); $$ = $5; }
-                   ;
-
-return_statement: tok_return ';' { debugBison(36); createReturnStatement(nullptr); $$ = nullptr; }
-                | tok_return expression ';' { debugBison(37); createReturnStatement($2); $$ = $2; }
-                ;
-
-expression: term                          { debugBison(10); $$ = $1; }
-          | expression '+' expression     { debugBison(11); $$ = performBinaryOperation($1, $3, '+'); }
-          | expression '-' expression     { debugBison(12); $$ = performBinaryOperation($1, $3, '-'); }
-          | expression '/' expression     { debugBison(13); $$ = performBinaryOperation($1, $3, '/'); }
-          | expression '*' expression     { debugBison(14); $$ = performBinaryOperation($1, $3, '*'); }
-          | expression '<' expression     { debugBison(38); $$ = createComparison($1, $3, '<'); }
-          | expression '>' expression     { debugBison(39); $$ = createComparison($1, $3, '>'); }
-          | expression tok_le expression  { debugBison(40); $$ = createComparison($1, $3, tok_le); }
-          | expression tok_ge expression  { debugBison(41); $$ = createComparison($1, $3, tok_ge); }
-          | expression tok_eq expression  { debugBison(42); $$ = createComparison($1, $3, tok_eq); }
-          | expression tok_ne expression  { debugBison(43); $$ = createComparison($1, $3, tok_ne); }
-          | '(' expression ')'            { debugBison(15); $$ = $2; }
-          ;
-
-term: tok_identifier                     { debugBison(7); Value* ptr = getFromSymbolTable($1); $$ = builder.CreateLoad(builder.getDoubleTy(), ptr, "load_identifier"); free($1); }
-    | tok_double_literal                 { debugBison(8); $$ = createDoubleConstant($1); }
-    | tok_identifier '(' argument_list ')' { debugBison(44); $$ = createFunctionCall($1, $3); free($1); }
-    ;
-
-argument_list: /* empty */                { debugBison(45); $$ = new std::vector<llvm::Value*>(); }
-            | expression                  { debugBison(46); $$ = new std::vector<llvm::Value*>(); $$->push_back($1); }
-            | argument_list ',' expression { debugBison(47); $1->push_back($3); $$ = $1; }
-            ;
-
-%%
-=======
 %token <op> tok_relop
 %token <identifier> tok_identifier
 %token <double_literal> tok_double_literal
@@ -267,4 +130,3 @@ int main(int argc, char** argv) {
     
     return parserResult;
 }
->>>>>>> master
